2e8ce7cc8360e830a120e00e0ce6199b809bb83e
Ulysse Carion <ulyssecarion@gmail.com>
2020-01-03 22:04:10 -0800

    mix new

---
+++ lib/jddf.ex
@@ -0,0 +1,18 @@
+defmodule JDDF do
+  @moduledoc """
+  Documentation for JDDF.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JDDF.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/jddf_test.exs
@@ -0,0 +1,8 @@
+defmodule JDDFTest do
+  use ExUnit.Case
+  doctest JDDF
+
+  test "greets the world" do
+    assert JDDF.hello() == :world
+  end
+end
